06162f88b2e1ba44dfdb5e9bbd7ffd22a111f99c
effedonati@gmail.com <effedonati@gmail.com>
2015-03-04 20:08:40 +0100

    download and read dataset

---
+++ download.R
@@ -0,0 +1,28 @@
+download_dataset<-function() {
+    filename<-"./household_power_consumption.zip"
+    fileurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(fileurl,destfile=filename,method="curl")
+    print("downloaded")
+    unzip(filename)
+    print("unzipped")
+    
+    
+}
+
+read_dataset<-function(){
+    library(plyr,warn.conflicts=FALSE)
+    library(lubridate)
+    filename="./household_power_consumption.txt"
+    rowstoskip=66637
+    rowstoread=2880
+    dset<-read.csv(file=filename,header=FALSE,sep=";",skip=rowstoskip,nrows=rowstoread)
+    colnames <- read.csv(filename, header = FALSE, sep =';', nrows = 1,,stringsAsFactors = FALSE)
+    names(dset)<-colnames
+    dset<-mutate(dset,Date=dmy(Date),Time=hms(Time))
+    dset
+    
+}
+plot1<-function(){
+    x<-read_dataset()
+    hist(x$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+}
